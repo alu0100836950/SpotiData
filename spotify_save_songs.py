c2ef8386cac52a8167de47f7e3420231f84e5d59
alu0100836950 <alu0100836950@ull.edu.es>
2020-06-04 13:15:30 +0100

    Añadiendo metodos para mostrar graficas

--- spotify_save_songs.py
+++ spotify_save_songs.py
@@ -3,6 +3,10 @@ import spotipy
 import spotipy.util as util
 import pandas as pd
 import csv
+import seaborn as sns
+import matplotlib.style as style
+import matplotlib.pyplot as plt
+import numpy as np
 
 #----------------SetUp Spotify------------------#
 scope = 'user-library-read'
@@ -10,6 +14,42 @@ token = util.prompt_for_user_token('alberto.mrtn',scope, client_id='23c9bd477825
 sp = spotipy.Spotify(auth=token)
 #----------------------------------#
 
+def show_pairplot(df, audio_feature_headers):
+    
+    
+    # Seaborn style
+    sns.set(style="white")
+    style.use('seaborn-poster') #sets the size of the charts
+    style.use('ggplot')
+
+
+    g =sns.pairplot(df[audio_feature_headers],height = 5, aspect = 0.5)
+    plt.show()
+
+def show_heatmap(df, audio_feature_headers):
+    
+    corr = df[audio_feature_headers].corr()
+
+    mask = np.zeros_like(corr, dtype=np.bool)
+    mask[np.triu_indices_from(mask)] = True
+
+    f, ax = plt.subplots(figsize=(11,9))
+
+    cmap = sns.diverging_palette(220,10, as_cmap=True)
+
+    sns.heatmap(corr, mask=mask, cmap=cmap, vmax =.6, vmin=-.4, center=0, square=True, linewidths=.5, cbar_kws={"shrink": .5})
+    plt.show()
+
+def show_relation(df):
+
+    # De esta forma tambien podemos ver los histogramas
+
+    #sns.jointplot(x='energy', y='valence', data=df, kind='reg')
+    #sns.jointplot(x='danceability', y='valence', data=df, kind='reg')
+
+    # Otra forma de ver la relacion de dos variables
+    sns.regplot(x='energy', y='acousticness', data=df)
+    plt.show()
 
 def getFeature(items):
     idList = []
@@ -43,21 +83,28 @@ if token:
     PLAYLIST_ID = '37i9dQZEVXbLoATJ81JYXz'
     items = sp.playlist_tracks(PLAYLIST_ID)['items']
 
+    audio_feature_headers = ['danceability', 'energy', 'speechiness', 'valence', 'mode','acousticness']
 
     features = getFeature(items)
 
     with open('features.csv', mode='w') as f:
         f.write('title,danceability,acousticness,energy,speechiness,valence,mode\n')
         for feature in features:
-            f.write(feature['title'].encode('utf-8') + ',' + str(feature['danceability']) + ',' + str(feature['acousticness'])
+            f.write(feature['title'] + ',' + str(feature['danceability']) + ',' + str(feature['acousticness']) #.encode('utf-8') a mi me peta si se lo pongo al title
             + ',' + str(feature['energy']) + ',' + str(feature['speechiness']) + ',' + str(feature['valence']) + ','
             + str(feature['mode']) + '\n')
 
 
     df = pd.read_csv('features.csv')
+    df = df.dropna()
     print(df)
     print(df.describe())
 
+#   ********mostramos las gráficas************
+
+    #show_pairplot(df, audio_feature_headers)
+    #show_heatmap(df,audio_feature_headers)
+    show_relation(df)
 
 else:
     print("Can't get token")
